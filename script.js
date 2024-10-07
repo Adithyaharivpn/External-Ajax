d4862654968637073639170bc6c99f48f15755db
Adithyahari vpn <68626434+Adithyaharivpn@users.noreply.github.com>
2024-10-07 14:38:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function get() {
+    //coliole.log("button clicked");
+  
+    //step 1
+    var xhttp = new XMLHttpRequest();
+    //step2
+    xhttp.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+    //step3
+    xhttp.send();
+    //step4
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        var json=JSON.parse(this.responseText);
+        let val="";
+        for (let i = 0; i <json.length; i++) {
+          val +=`<tr><td>${json[i].name}</td><td>${json[i].username}</td><td>${json[i].address.city}</td></tr>`;
+          document.getElementById("table").innerHTML = val;    
+        } 
+      }
+    };
+  }
+  
